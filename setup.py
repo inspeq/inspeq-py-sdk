7d4542e367c52de2dd48e31f1a6a69606ad1c870
sharmashubam <shubam39992355@gmail.com>
2024-02-07 18:40:58 +0530

    remvoed project_urls

--- setup.py
+++ setup.py
@@ -1,15 +1,9 @@
 from setuptools import setup, find_packages
 
-
-def read_file(file_name):
-    with open(file_name, 'r', encoding='utf-8') as file:
-        return file.read()
-
-
 setup(
     name="inspeqai",
     version="1.0.0",
-    packages=find_packages(include=['inspeq.*','inspeq']),
+    packages=find_packages(),
     license="Apache 2.0",
     author="Inspeq",
      install_requires=[
@@ -17,7 +11,6 @@ setup(
             # Specify dependencies if any
     ],
     description="Inspeq AI SDK",
-    long_description=read_file(file_name="README.md"),
-    python_requires=">=3.10",
-      
+
+    python_requires=">=3.10",  
 )
