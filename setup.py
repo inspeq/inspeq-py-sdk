44ef5e5f2aa0c6d0a5e70e4482171cbf2fe82a83
sharmashubam <shubam39992355@gmail.com>
2024-02-07 20:01:23 +0530

    changed version

--- setup.py
+++ setup.py
@@ -4,7 +4,7 @@ with open('README.md', 'r') as f:
 
 setup(
     name="inspeqai",
-    version="1.0.1",
+    version="1.0.2",
     packages=find_packages(include=["inspeq*"]),
     license="Apache 2.0",
     author="Inspeq",
