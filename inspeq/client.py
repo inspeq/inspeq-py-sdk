13a6c4e96161ec3de3df670548533482e6fbfc60
Shubam Sharma <shubam39992355@gmail.com>
2024-03-29 20:41:19 +0530

    new-sdk-features

--- inspeq/client.py
+++ inspeq/client.py
@@ -15,8 +15,10 @@ class Evaluator:
         self.sdk_api_key = sdk_api_key
         self.API_URL = api_url if api_url else API_URL
 
-    def make_api_request(self, endpoint, input_data):
+    def make_api_request(self, endpoint, input_data, config_input:None, task_name:None):
         url = f"{self.API_URL}/api/v1/sdk/{endpoint}"
+        
+
         prompt = input_data.get("prompt", "")
         context = input_data.get("context", "")
         response = input_data.get("response", "")
@@ -24,13 +26,14 @@ class Evaluator:
         try:
             response = requests.post(
                 url,
-                params={"secret_key": self.sdk_api_key},
+                params={"secret_key": self.sdk_api_key, "task_name": task_name},
                 json={
                     "input_data": {
                         "llm_input_query": prompt,
                         "llm_input_context": context,
                         "llm_output": response,
-                    }
+                    },
+                    "config_input":config_input
                 },
             )
             response.raise_for_status()
@@ -47,7 +50,7 @@ class Evaluator:
                 print(f"HTTP Error: {err}")
             return None
 
-    def word_limit_test(self, input_data):
+    def word_limit_test(self, input_data,  config_input, task_name= None):
         # Validate input_data
         response_data = input_data.get("response", "").strip()
         if not response_data:
@@ -56,7 +59,7 @@ class Evaluator:
         if not response_data:
             raise ValueError("'prompt' is not provided")
 
-        return self.make_api_request("word_limit_test", input_data)
+        return self.make_api_request("word_limit_test", input_data,config_input, task_name)
 
     # def grammatical_correctness(self, input_data):
     #     # Validate input_data
@@ -66,13 +69,14 @@ class Evaluator:
 
     #     return self.make_api_request("grammatical_correctness", input_data)
 
-    def do_not_use_keywords(self, input_data):
+
+    def do_not_use_keywords(self, input_data,config_input, task_name= None):
         # Validate input_data
         response_data = input_data.get("response", "").strip()
         if not response_data:
             raise ValueError("'response' is not provided")
 
-        return self.make_api_request("do_not_use_keywords", input_data)
+        return self.make_api_request("do_not_use_keywords", input_data, config_input, task_name)
 
     # def fluency(self, input_data):
     #     # Validate input_data
@@ -82,8 +86,9 @@ class Evaluator:
 
     #     return self.make_api_request("answer_fluency", input_data)
 
-    def answer_relevance(self, input_data):
+    def answer_relevance(self, input_data, config_input, task_name= None):
         # Validate input_data
+        print(task_name)
         response_data = input_data.get("response", "").strip()
         if not response_data:
             raise ValueError("'response' is not provided")
@@ -91,28 +96,18 @@ class Evaluator:
         if not response_data:
             raise ValueError("'prompt' is not provided")
 
-        return self.make_api_request("answer_relevance", input_data)
+        return self.make_api_request("answer_relevance", input_data, config_input, task_name)
 
-    def response_tone(self, input_data):
+    def response_tone(self, input_data, config_input, task_name= None):
+
         # Validate input_data
         response_data = input_data.get("response", "").strip()
         if not response_data:
             raise ValueError("'response' is not provided")
 
-        return self.make_api_request("response_tone", input_data)
+        return self.make_api_request("response_tone", input_data, config_input, task_name)
 
-    def factual_consistency(self, input_data):
-        # Validate input_data
-        response_data = input_data.get("response", "").strip()
-        if not response_data:
-            raise ValueError("'response' is not provided")
-        response_data = input_data.get("context", "").strip()
-        if not response_data:
-            raise ValueError("'context' is not provided")
-
-        return self.make_api_request("factual_consistency", input_data)
-
-    def conceptual_similarity(self, input_data):
+    def factual_consistency(self, input_data, config_input, task_name= None):
         # Validate input_data
         response_data = input_data.get("response", "").strip()
         if not response_data:
@@ -121,25 +116,9 @@ class Evaluator:
         if not response_data:
             raise ValueError("'context' is not provided")
 
-        return self.make_api_request("conceptual_similarity", input_data)
+        return self.make_api_request("factual_consistency", input_data, config_input, task_name)
 
-    def clarity(self, input_data):
-        # Validate input_data
-        response_data = input_data.get("response", "").strip()
-        if not response_data:
-            raise ValueError("'response' is not provided")
-
-        return self.make_api_request("clarity", input_data)
-
-    def readability(self, input_data):
-        # Validate input_data
-        response_data = input_data.get("response", "").strip()
-        if not response_data:
-            raise ValueError("'response' is not provided")
-
-        return self.make_api_request("readability", input_data)
-
-    def coherence(self, input_data):
+    def conceptual_similarity(self, input_data, config_input, task_name= None):
         # Validate input_data
         response_data = input_data.get("response", "").strip()
         if not response_data:
@@ -148,33 +127,25 @@ class Evaluator:
         if not response_data:
             raise ValueError("'context' is not provided")
 
-        return self.make_api_request("coherence", input_data)
+        return self.make_api_request("conceptual_similarity", input_data, config_input, task_name)
 
-    def model_refusal(self, input_data):
+    def clarity(self, input_data, config_input, task_name= None):
         # Validate input_data
         response_data = input_data.get("response", "").strip()
         if not response_data:
             raise ValueError("'response' is not provided")
 
-        return self.make_api_request("model_refusal", input_data)
+        return self.make_api_request("clarity", input_data, config_input, task_name)
 
-    def data_leakage(self, input_data):
+    def readability(self, input_data, config_input, task_name= None):
         # Validate input_data
         response_data = input_data.get("response", "").strip()
         if not response_data:
             raise ValueError("'response' is not provided")
 
-        return self.make_api_request("data_leakage", input_data)
+        return self.make_api_request("readability", input_data, config_input, task_name)
 
-    def diversity(self, input_data):
-        # Validate input_data
-        response_data = input_data.get("response", "").strip()
-        if not response_data:
-            raise ValueError("'response' is not provided")
-
-        return self.make_api_request("diversity", input_data)
-
-    def creativity(self, input_data):
+    def coherence(self, input_data, config_input, task_name= None):
         # Validate input_data
         response_data = input_data.get("response", "").strip()
         if not response_data:
@@ -183,40 +154,75 @@ class Evaluator:
         if not response_data:
             raise ValueError("'context' is not provided")
 
-        return self.make_api_request("creativity", input_data)
+        return self.make_api_request("coherence", input_data, config_input, task_name)
 
-    def narrative_continuity(self, input_data):
+    def model_refusal(self, input_data, config_input, task_name= None):
+        # Validate input_data
+        response_data = input_data.get("response", "").strip()
+        if not response_data:
+            raise ValueError("'response' is not provided")
+
+        return self.make_api_request("model_refusal", input_data, config_input, task_name)
+
+    def data_leakage(self, input_data, config_input, task_name= None):
+        # Validate input_data
+        response_data = input_data.get("response", "").strip()
+        if not response_data:
+            raise ValueError("'response' is not provided")
+
+        return self.make_api_request("data_leakage", input_data, config_input, task_name)
+
+    def diversity(self, input_data, config_input, task_name= None):
+        # Validate input_data
+        response_data = input_data.get("response", "").strip()
+        if not response_data:
+            raise ValueError("'response' is not provided")
+
+        return self.make_api_request("diversity", input_data, config_input, task_name)
+
+    def creativity(self, input_data, config_input, task_name= None):
+        # Validate input_data
+        response_data = input_data.get("response", "").strip()
+        if not response_data:
+            raise ValueError("'response' is not provided")
+        response_data = input_data.get("context", "").strip()
+        if not response_data:
+            raise ValueError("'context' is not provided")
+
+        return self.make_api_request("creativity", input_data, config_input, task_name)
+
+    def narrative_continuity(self, input_data, config_input, task_name= None):
         # Validate input_data
         response_data = input_data.get("response", "").strip()
         if not response_data:
             raise ValueError("'response' is not provided")
         response_data = input_data.get("context", "").strip()
 
-        return self.make_api_request("narrative_continuity", input_data)
+        return self.make_api_request("narrative_continuity", input_data, config_input, task_name)
 
-    def get_all_metrics(self, input_data):
+    def get_all_metrics(self, input_data,config_input):
         metrics_results = {}
 
-        metrics_results["factual_consistency"] = self.factual_consistency(input_data)
-        metrics_results["answer_relevance"] = self.answer_relevance(input_data)
-        metrics_results["response_tone"] = self.response_tone(input_data)
+        metrics_results["factual_consistency"] = self.factual_consistency(input_data,config_input, task_name= None)
+        metrics_results["answer_relevance"] = self.answer_relevance(input_data ,config_input, task_name= None)
+        metrics_results["response_tone"] = self.response_tone(input_data ,config_input, task_name= None)
         # metrics_results["grammatical_correctness"] = self.grammatical_correctness(
         #     input_data
         # )
         # metrics_results["fluency"] = self.fluency(input_data)
-        metrics_results["do_not_use_keywords"] = self.do_not_use_keywords(input_data)
-        metrics_results["word_limit_test"] = self.word_limit_test(input_data)
+        metrics_results["do_not_use_keywords"] = self.do_not_use_keywords(input_data ,config_input, task_name= None)
+        metrics_results["word_limit_test"] = self.word_limit_test(input_data ,config_input, task_name= None)
         metrics_results["conceptual_similarity"] = self.conceptual_similarity(
-            input_data
+            input_data ,config_input, task_name= None
         )
-        metrics_results["coherence"] = self.coherence(input_data)
-        metrics_results["readability"] = self.readability(input_data)
-        metrics_results["clarity"] = self.clarity(input_data)
-        metrics_results["model_refusal"] = self.model_refusal(input_data)
-        metrics_results["data_leakage"] = self.data_leakage(input_data)
-        metrics_results["diversity"] = self.diversity(input_data)
-        metrics_results["creativity"] = self.creativity(input_data)
-        metrics_results["narrative_continuity"] = self.narrative_continuity(input_data)
+        metrics_results["coherence"] = self.coherence(input_data ,config_input, task_name= None)
+        metrics_results["readability"] = self.readability(input_data ,config_input, task_name= None)
+        metrics_results["clarity"] = self.clarity(input_data ,config_input, task_name= None)
+        metrics_results["model_refusal"] = self.model_refusal(input_data ,config_input, task_name= None)
+        metrics_results["data_leakage"] = self.data_leakage(input_data ,config_input, task_name= None)
+        metrics_results["diversity"] = self.diversity(input_data ,config_input, task_name= None)
+        metrics_results["creativity"] = self.creativity(input_data ,config_input, task_name= None)
+        metrics_results["narrative_continuity"] = self.narrative_continuity(input_data ,config_input, task_name= None)
 
         return metrics_results
 
@@ -264,7 +270,7 @@ class Eval:
             return processed_data
 
         else:
-            return f"Error: {response.status_code} - {response.text}"
+            return f"{response.text}"
 
     # def eval(self, input_data, config_input=None, metrics_list=None):
     #     url = f"{self.API_URL}/api/v1/sdk/eval"
--- test.py
+++ test.py
@@ -2,7 +2,7 @@ from inspeq.client import Evaluator
 from inspeq.client import Eval
 
 # old way to call th metrics
-API_KEY = "sdk_api-key"
+API_KEY = "in-f579f248eb74d2bbf5c4fa40bac03298"
 inspeq_instance = Evaluator(sdk_api_key=API_KEY)
 
 input_data =    {
@@ -10,27 +10,50 @@ input_data =    {
     "context": "string",
     "response": "string"
   }
-result=inspeq_instance.word_limit_test(input_data)
+
+config_input= {
+        "threshold": 0.5,
+        "custom_labels": ["string","dsfd"],
+        "label_thresholds": [0,0.5],
+    }
+
+result=inspeq_instance.data_leakage(input_data,config_input, task_name="test_task")
 print(result)
 
-# Eval new way to call the metrics
-
+#
 prompt = "query_text" # these are required fields depending on metrics
 context = "context_text"  # these are required fields depending on metrics
 response = "output_text"  # these are required fields depending on metrics
-API_KEY = "sdk_api_key" # it is required field
 task_name = "abc" #it is compulsory to provide whatever you want to give the task name.
 
 metrics_list = [
-    "DIVERSITY"
+    "DIVERSITY",
+    "Answer_relevance",
+    "conceptual_similarity"
 ]
 metrics_config = {
     "diversity_config": {
         "threshold": 0.5,
         "custom_labels": ["string"],
         "label_thresholds": [0],
+    },
+    "conceptual_similarity_config": {
+        "threshold": 0.5,
+        "custom_labels": ["string"],
+        "label_thresholds": [0],
     }
 }
-inspeq_instance = Eval(API_KEY)
-result = inspeq_instance.evaluate_task(prompt, context, response, task_name, metrics_list, metrics_config)
-print(result)
+# inspeq_instance = Eval(API_KEY)
+# result = inspeq_instance.evaluate_task(prompt, context, response, task_name, metrics_list, metrics_config)
+# print(result)
+
+
+payload = {
+            "metrics_list": metrics_list,
+            "input_data": {
+                "llm_input_query": prompt,
+                "llm_input_context": context,
+                "llm_output": response,
+            },
+            "metrics_config": metrics_config or {},
+        }
